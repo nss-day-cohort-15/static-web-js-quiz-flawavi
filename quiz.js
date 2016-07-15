fd6faa10595660b3a0b1cc83133f588abc76757f
Fletcher Watson <fbwthe6@gmail.com>
2016-07-15 10:46:24 -0500

    initial commit. created object variable, html shell, and outlined function

---
+++ quiz.js
@@ -0,0 +1,13 @@
+var treeBuilder {
+  height: document.getElementById("height")
+  character: document.getElementById("character")
+}
+
+function tree (obj) {
+  //Create a tree function that should build a pine tree
+  //out of a character in the Chrome dev tools console.
+  //It accepts a single object as an argument.
+  //The object should have two key/value pairs.
+
+
+}
