d20f1f6d2d489d36908bea034a4253e2c5b5ce37
Fletcher Watson <fbwthe6@gmail.com>
2016-07-25 13:19:31 -0500

    added button listener for return key.

--- quiz.js
+++ quiz.js
@@ -1,13 +1,33 @@
-var treeBuilder {
-  height: document.getElementById("height")
-  character: document.getElementById("character")
-}
 
-function tree (obj) {
-  //Create a tree function that should build a pine tree
-  //out of a character in the Chrome dev tools console.
-  //It accepts a single object as an argument.
-  //The object should have two key/value pairs.
+///////////////////////////// SCRATCH 3 /////////////////////////////
+
+var button = document.querySelector('#form input[type=submit]');
+var lines;
+var spaces;
+var spaceCount = 0;
 
 
+button.addEventListener('click', function (evt) {
+  var height = document.querySelector('#height').value;
+  var character = document.querySelector('#character').value;
+  if ((height && character) && (height > 0)) {
+    buildTree(height, character);
+  } else {
+    alert("Please enter a height of 1 or more, and a character of your choosing. :)");
+  }
+  evt.preventDefault();
+});
+
+function buildTree(height, character) {
+  for (var i = 1; i <= height;) {
+    spaceCount = height - i;
+    line = Array(i*2).join(character);
+    if (spaceCount > 0) {
+      spaces = Array(spaceCount+1).join(' ');
+      console.log(spaces+''+line);
+    } else {
+      console.log(line);
+    }
+    i++;
+  }
 }
